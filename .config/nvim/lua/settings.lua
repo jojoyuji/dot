5345c00abe783fae5e2bcdbb29176fe2a91e47c0
Jojo Hashimoto <jojo@MacBook-de-Jojo.local>
2021-04-05 01:13:50 -0400

    more lua project structure change

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -1,2 +1,3 @@
-vim.fn.eval("execute ('so ~/.config/nvim/pluginsrc')")
-vim.fn.eval("execute ('so ~/.config/nvim/mappingsrc')")
+vim.fn.eval("execute ('so ~/.config/nvim/lua/pluginsrc')")
+vim.fn.eval("execute ('so ~/.config/nvim/lua/config')")
+vim.fn.eval("execute ('so ~/.config/nvim/lua/mappingsrc')")
